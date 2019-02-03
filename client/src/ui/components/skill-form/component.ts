82e9127e280afd7ba7bf5fbec66fb03c1e14fe67
gossi <gossi@users.noreply.github.com>
2019-02-03 13:49:10 +0100

    fix/workaround infinite rendering loop with e-p-s

--- client/src/ui/components/skill-form/component.ts
+++ client/src/ui/components/skill-form/component.ts
@@ -1,13 +1,10 @@
 import { inject as service } from '@ember-decorators/service';
-import Group from '@spomoda/client/src/data/models/group';
-import Instrument from '@spomoda/client/src/data/models/instrument';
 import Skill, { SkillType } from '@spomoda/client/src/data/models/skill';
 import Sport from '@spomoda/client/src/data/models/sport';
 import WolkenkitService from '@spomoda/client/src/services/wolkenkit';
 import Task from 'ember-concurrency/task';
 import SparklesComponent, { tracked } from 'sparkles-component';
 import { arg } from 'sparkles-decorators';
-import { observes } from '@ember-decorators/object';
 
 interface SkillFormArgs {
 	sport: Sport;
@@ -18,7 +15,12 @@ interface SkillFormArgs {
 export default class SkillFormComponent extends SparklesComponent<SkillFormArgs> {
 	@service wolkenkit!: WolkenkitService;
 
-	@arg skill: Skill = Skill.create({});
+	@arg skill: Skill = Skill.create({
+		relationships: {
+			parents: [],
+			parts: []
+		}
+	});
 
 	types = {
 		[SkillType.SKILL]: SkillType.SKILL,
@@ -26,36 +28,40 @@ export default class SkillFormComponent extends SparklesComponent<SkillFormArgs>
 		[SkillType.MULTIPLE]: SkillType.MULTIPLE
 	};
 
-	@tracked instruments?: Instrument[];
-	@tracked groups?: Group[];
-	@tracked skills?: Skill[];
+	// Workaround (see below)
+	@tracked multiples?: Skill[];
+	@tracked options?: Skill[];
 
-	@tracked options: Skill[] = [];
+	latestSkills?: Skill[];
+
+	constructor(args: SkillFormArgs) {
+		super(args);
+		this.setOptions();
+	}
+
+	didUpdate() {
+		this.setOptions();
+	}
+
+	setOptions() {
+		if (this.latestSkills !== this.args.sport.skills || !this.options || !this.multiples) {
+			this.options = this.args.sport.skills.filter(skill => skill !== this.skill).sortBy('name');
+			this.multiples = this.args.sport.skills.filter(skill => skill.type === SkillType.MULTIPLE).sortBy('name');
+			this.latestSkills = this.args.sport.skills;
+		}
+	}
+
+	// Intended Implementation
+	// Related issue: https://github.com/cibernox/ember-power-select/issues/1196
 
 	// @tracked('args')
-	@observes('args')
-	optionsObserver() {
-		// this.options = this.args.sport.skills.slice().filter(skill => skill !== this.skill).sortBy('name');
-		this.options = [];
-	}
 	// get options(): Skill[] {
-	// 	return [];
-	// 	// return this.args.sport.skills.slice().filter(skill => skill !== this.skill).sortBy('name');
+	// 	return this.args.sport.skills.filter(skill => skill !== this.skill).sortBy('name');
 	// }
 
-	@tracked('args')
-	get multiples(): Skill[] {
-		return this.args.sport.skills.filter(skill => skill.type === SkillType.MULTIPLE).sortBy('name');
-	}
-
-	// async didInsertElement() {
-	// 	this.groups = await this.wolkenkit.live('groups', { where: { sportId: this.args.sport.id } });
-	// 	this.instruments = await this.wolkenkit.live('instruments', { where: { sportId: this.args.sport.id } });
-	// 	this.skills = await this.wolkenkit.read('skills', { where: { sportId: this.args.sport.id } });
+	// @tracked('args')
+	// get multiples(): Skill[] {
+	// 	return this.args.sport.skills.filter(skill => skill.type === SkillType.MULTIPLE).sortBy('name');
 	// }
-
-	void() {
-
-	}
 }
 
